5ee69d18e6c8b2a63f28eb2ab733de131323c1a7
Aditha Doratiyawa <indegamedev@gmail.com>
2026-01-04 13:02:32 +1100

    updated homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<div class="w-1/3 p-8">
-	<h1>Welcome to Job app</h1>
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="flex min-h-screen items-center justify-center">
+	<div class="w-/3 flex flex-col items-center space-y-8 p-8">
+		<h1 class="text-center text-6xl font-bold">Welcome to Job app</h1>
+		<Button href="/signin" class="">Login</Button>
+	</div>
 </div>
