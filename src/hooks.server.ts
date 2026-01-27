4fb420694f172e2b8c0afc38418ff6d1dfd71c44
Aditha Doratiyawa <indegamedev@gmail.com>
2026-01-27 22:05:30 +1000

    fixed cloudflare configuration

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,35 +3,27 @@ import { svelteKitHandler } from 'better-auth/svelte-kit';
 import { building } from '$app/environment';
 import { getDb } from './lib/server/db';
 import { getAuth } from '$lib/server/auth';
-import { S3Client } from '@aws-sdk/client-s3';
 
 const handler: Handle = async ({ event, resolve }) => {
+	try {
+		event.locals.db = getDb(event.platform?.env.job_app_db, event.platform?.env.DATABASE_URL);
+		event.locals.auth = getAuth(event.locals.db);
 
-	event.locals.db = getDb(event.platform?.env.job_app_db, event.platform?.env.DATABASE_URL);
+		const session = await event.locals.auth.api.getSession({
+			headers: event.request.headers,
+		});
 
-	if (!event.platform) throw Error("Platform details missing");
-
-	event.locals.storage = new S3Client({
-		region: "auto",
-		endpoint: event.platform?.env.R2_URL,
-		credentials: {
-			accessKeyId: event.platform?.env.R2_KEY_ID,
-			secretAccessKey: event.platform?.env.R2_SECRET_ACCESS_KEY
+		if (session) {
+			event.locals.session = session.session;
+			event.locals.user = session.user;
 		}
-	});
 
-	event.locals.auth = getAuth(event.locals.db);
 
-	const session = await event.locals.auth.api.getSession({
-		headers: event.request.headers,
-	});
-
-	if (session) {
-		event.locals.session = session.session;
-		event.locals.user = session.user;
+		return svelteKitHandler({ event, resolve, auth: event.locals.auth, building });
+	} catch (error) {
+		console.log(error)
+		throw error;
 	}
-
-	return svelteKitHandler({ event, resolve, auth: event.locals.auth, building });
 };
 
 export const handle: Handle = handler;
